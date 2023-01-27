8aea11266648900921bb991345b32e5f3432a310
SuvarneshKM <suvarnesh1729@gmail.com>
2023-01-27 19:26:56 +0530

    added hoomans logo animation

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Bouncing_Ball from './bouncing_ball'
 import Hamburger from './hamburger'
+import Hoomans_Logo from './hoomans_logo'
 
 export default function Projects() {
   return (
     <div className='flex flex-row gap-10 lg:gap-y-20 gap-y-20 justify-center flex-wrap'>
       <Bouncing_Ball />
       <Hamburger />
+      <Hoomans_Logo />
     </div>
   )
 }
